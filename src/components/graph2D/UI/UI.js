509168b0ff351a7dd441629acc232f878dbe7504
mmA1r <mask.matveev@mail.ru>
2022-04-25 15:56:28 +0400

    UPD

--- src/components/graph2D/UI/UI.js
+++ src/components/graph2D/UI/UI.js
@@ -1,12 +1,30 @@
 import React from "react";
-import AddFunction from "./AddFunction";
-import '../UI/UIComponents/UI.css'
+import FuncInputs from "../funcInputs/FuncInputs";
 
 class UI extends React.Component {
+    constructor(props) {
+        super(props);
+        const { funcs, addFunction, delFunction, run } = props;
+        this.addFunction = addFunction;
+        this.delFunction = delFunction;
+        this.funcs = funcs;
+        this.run = run;
+    }
+
     render() {
-        return(
-            <div className = "UI">
-                <AddFunction></AddFunction>
+        return (
+            <div>
+                <button onClick = {() => this.addFunction()}>Add Function</button>
+                <div>
+                    { this.funcs.map((func, index) =>
+                        <FuncInputs 
+                            key = { index }
+                            func = { func }
+                            delFunction = {(index) => this.delFunction(index)}
+                            run = {() => this.run()}
+                        ></FuncInputs>
+                    )}
+                </div>
             </div>
         );
     }
